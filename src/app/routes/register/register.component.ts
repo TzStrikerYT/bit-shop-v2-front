e3ff3da250e111d3e0589957bfacc0758f629f8f
Striker <striker@pop-os.localdomain>
2023-05-30 21:21:58 -0500

    [add] add new user

--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -5,23 +5,18 @@ import { UserService } from 'src/app/api/users/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(public userService: UserService) {}
 
-  constructor(public userService: UserService){
-
-  }
-
-  createUser(form: NgForm){
-
+  createUser(form: NgForm) {
     // revisar los campos
-    let data = form.value
+    let data = form.value;
 
     this.userService.createUser(data).subscribe((data: any) => {
-      console.log(data)
-      alert(data.status > 399 ? data.error.msg : data.msg)
-    })
+      console.log(data);
+      alert(data.status > 399 ? data.error.msg : data.msg);
+    });
   }
-
 }
