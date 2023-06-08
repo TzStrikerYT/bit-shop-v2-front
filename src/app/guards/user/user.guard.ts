4644e9b5f03027a74557dfa321e6c373f9f69205
Striker <striker@pop-os.localdomain>
2023-06-07 20:51:14 -0500

    add guards

--- src/app/guards/user/user.guard.ts
+++ src/app/guards/user/user.guard.ts
@@ -3,14 +3,9 @@ import { CanActivateFn, Router } from '@angular/router';
 export const userGuard: CanActivateFn = (route, state) => {
 
   const router = new Router()
-  let st = 2
+  let st = localStorage.getItem('token')
   
-  if(st === 1) return isLoggedIn();
+  if(st) return true;
   router.navigate(['/login'])
   return false
 };
-
-const isLoggedIn = () => {
-  console.log(localStorage.getItem('token'))
-  return localStorage.getItem('token') ? true : false
-}
